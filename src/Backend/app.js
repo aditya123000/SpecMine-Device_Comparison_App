9847bb9da4779e4c8122a9049b75865fe59d1173
aditya123000 <aditdwivedi336@gmail.com>
2026-04-07 22:36:05 +0530

    fix: cors preflight for auth routes

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -35,17 +35,14 @@ const isOriginAllowed = (origin) =>
   });
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || isOriginAllowed(origin)) {
-      callback(null, true);
-    } else {
-      const corsError = new Error("Not allowed by CORS");
-      corsError.status = 403;
-      callback(corsError);
-    }
-  },
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+app.options("*", cors());
+
 app.use(express.json());
 app.use(logger);
 
